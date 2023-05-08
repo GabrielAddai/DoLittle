bc9c0260edddb152226b06f9b3e58f8ce959463f
Kishan Patel <kpatel49@students.towson.edu>
2023-05-08 12:15:03 -0400

    search.js file added for API

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3af4d40f1bb4bbd7"; }
+/******/ 		__webpack_require__.h = function() { return "015ad4a19558306a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/pages/api/search.js
@@ -0,0 +1,59 @@
+  
+//search API
+const fetch = require("node-fetch");
+
+// app key are for my personal account
+//Google app key 
+const googleSecret = "AIzaSyCB-X8PwkNc9VFb1CC6MAu22hykjgbSmvk"; 
+
+//GPT app key
+const gptSecret = "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"; 
+
+//chat GPT
+const config = new Configuration({ apiKey: gptSecret });
+const openai = new OpenAIApi(config);
+
+function gpt (searchPrompt){
+
+    const response = openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: searchPrompt,
+        temperature: 0,
+        max_tokens: 1000, 
+      });
+    
+      // can be change into return statment as needed
+      response.then(res => {
+        //console.log(res);
+        //console.log(res.data.choices[0].text);
+      });
+}
+
+
+
+// youtube 
+const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${googleSecret}`;
+    
+async function youtube (searchPrompt, videoNum){
+    const url1 = `${youtubeURL}&type=video&part=snippet&q=${searchPrompt}&maxResults=${videoNum}`;
+    
+    const response1 =  await fetch(url1);
+    const data = await response1.json();
+    //can be change into return statment as needed
+    //console.log(data);
+
+}
+
+//google
+// cx code my ID for costome search API . 
+const googleURL = `https://www.googleapis.com/customsearch/v1?key=${googleSecret}&cx=26f7a2e0f129f447d`;
+    
+async function googlsearch (searchPrompt){
+    const url1 = `${googleURL}&q=${searchPrompt}`;
+    
+    const response1 =  await fetch(url1);
+    const data = await response1.json();
+    //can be change into return statment as needed
+    //console.log(data);
+
+}
