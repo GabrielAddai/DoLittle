641bf481ccc165a13ca5742600e1289e44394239
Tyreik Shaw <57421574+Tyreitix@users.noreply.github.com>
2023-05-15 02:43:46 -0400

    Progression for page navigation(Still has errors)

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,15 +2,33 @@ import React from 'react';
 import Login from '/components/LoginForm';
 import Registration from '/components/RegistrationForm';
 import Homepage from '/components/HomePageForm';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {  createBrowserRouter, RouterProvider, BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import About from './about';
 
 
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Homepage />,
+}]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 function App() {
   return (
-    <div>
-    <Login/>
+    <Router>
+    <div className='App'>
+      <Switch>
+        <Route path='/' component = {Homepage}/>
+          <Route path="/sign-in" exact component={<Login />} />
+          <Route path="/sign-up" element={<Registration />} />
+          <Route path="/about" element={<About />} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 export default App;
